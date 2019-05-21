9d35edd45c5dea4719cb864bed65d0b20b8efb4d
Jim Disenstam <jim.disenstam@gmail.com>
2019-05-21 10:21:47 +0200

    Implementing Deck class

--- TwentyOne/Application.cs
+++ TwentyOne/Application.cs
@@ -15,8 +15,24 @@ namespace TwentyOne
         {
             try
             {
+                List<Card> hand = new List<Card>();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    hand.Add(Deck.RemoveCard());
+                }
+
+                Deck.AddToDiscardPile(hand);
+
                 List<Card> stock = Deck.getStock();
+                Console.WriteLine("Stock:");
                 foreach (Card card in stock)
+                {
+                    Console.WriteLine(card.ToString());
+                }
+                List<Card> pile = Deck.getPile();
+                Console.WriteLine("Pile:");
+                foreach (Card card in pile)
                 {
                     Console.WriteLine(card.ToString());
                 }
--- TwentyOne/Deck.cs
+++ TwentyOne/Deck.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 namespace TwentyOne
 {
     /// <summary>
@@ -8,7 +9,7 @@ namespace TwentyOne
     public static class Deck
     {
         private static List<Card> _stock = new List<Card>();
-        private static List<Card> _discardPile;
+        private static List<Card> _discardPile = new List<Card>();
 
         /// <summary>
         ///     Initializes the Deck class.
@@ -22,12 +23,65 @@ namespace TwentyOne
                     Card card = new Card(suit, i);
                     _stock.Add(card);
                 }
+                ShuffleStock();
             }
         }
 
+        /// <summary>
+        ///     Removes and returns the last Card item from _stock.
+        ///     If there is only one Card left in _stock, it is refilled from _discardPile and shuffled.
+        /// </summary>
+        /// <returns>The last card from _stock.</returns>
+        public static Card RemoveCard()
+        {
+            Card topCard = _stock.Last();
+            _stock.Remove(topCard);
+            if (_stock.Count == 1)
+            {
+                _stock.AddRange(_discardPile);
+                ShuffleStock();
+            }
+
+            return topCard;
+        }
+
+        /// <summary>
+        ///     Adds an array of Cards to _discardPile.
+        /// </summary>
+        /// <param name="hand">Array of Cards to be added to _discardPile.</param>
+        public static void AddToDiscardPile(List<Card> hand)
+        {
+            _discardPile.AddRange(hand);
+        }
+        // Instantiates a new instance of Random.
+        private static Random rng = new Random();
+
+        /// <summary>
+        ///     Shuffles elements in _stock base on the Fisher-Yates shuffle.
+        ///     Solution adapted from: https://stackoverflow.com/questions/273313/randomize-a-listt
+        /// </summary>
+        private static void ShuffleStock()
+        {
+            int n = _stock.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card value = _stock[k];
+                _stock[k] = _stock[n];
+                _stock[n] = value;
+            }
+        }
+
+        // FOR TESTING - REMOVE LATER!
         public static List<Card> getStock()
         {
             return _stock;
+
+        }
+        public static List<Card> getPile()
+        {
+            return _discardPile;
 
         }
     }
